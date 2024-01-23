69167175eb65c65aa578946cf2f221412d212ba6
Praca <Praca@PRACA>
2024-01-23 20:36:51 +0100

    Zadanie domowe dz.6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,103 +1,48 @@
-﻿//// ========================= jeden sposób =============
+﻿using ChallengeApp;
 
-//List<string> daysOfWeeks = new List<string>();
+Employee empoylee1 = new Employee("Adrian","Mach",26);
+Employee employee2 = new Employee("Adam", "Kami", 31);
+Employee employee3 = new Employee("Olaf","Lubaszenko", 50);
 
-//daysOfWeeks.Add("poniedziałek");
-//daysOfWeeks.Add("wtorek");
-//daysOfWeeks.Add("sroda");
-//daysOfWeeks.Add("czwartek");
-//daysOfWeeks.Add("piatek");
-//daysOfWeeks.Add("sobota");
-//daysOfWeeks.Add("niedziela");
-//// pierwsza wersja for / foreach
-//for (int i = 0; i < daysOfWeeks.Count; i++)
-//{
-//    Console.WriteLine(daysOfWeeks[i]);
-//}
+empoylee1.AddScore(5);
+empoylee1.AddScore(4);
+empoylee1.AddScore(1);
+empoylee1.AddScore(3);
+empoylee1.AddScore(1);
 
-//// =+++==Drugi sposób ===++++
+employee2.AddScore(2);
+employee2.AddScore(8);
+employee2.AddScore(1);
+employee2.AddScore(6);
+employee2.AddScore(4);
 
-////string[] daysofweeks2 = { "pon", "wt", "śr", "czw", "pt", "sb", "nd" };
-////console.writeline(daysofweeks2[3]);
+employee3.AddScore(1);
+employee3.AddScore(4);
+employee3.AddScore(1);
+employee3.AddScore(5);
+employee3.AddScore(9);
 
-
-//// druga wersja foreach / for
-//foreach (var day in daysOfWeeks)
-//{
-//    Console.WriteLine(day);
-//}
-
-
-
-// Zadanie domowe
-
-int number = 45606;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
-
-foreach (char letter in letters)
+List<Employee> employees = new List<Employee>()
 {
-    if (letter == '0')
+    empoylee1,employee2, employee3,
+};
+
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
     {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result;
     }
 }
 
-
-Console.WriteLine("00==" + counter0);
-Console.WriteLine("01==" + counter1);
-Console.WriteLine("02==" + counter2);
-Console.WriteLine("03==" + counter3);
-Console.WriteLine("04==" + counter4);
-Console.WriteLine("05==" + counter5);
-Console.WriteLine("06==" + counter6);
-Console.WriteLine("07==" + counter7);
-Console.WriteLine("08==" + counter8);
-Console.WriteLine("09==" + counter9);
+Console.WriteLine(empoylee1.Name + "" + empoylee1.Surname + " lat " + empoylee1.Age + "wynik " + empoylee1.Result);
+Console.WriteLine(employee2.Name + "" + employee2.Surname + " lat " + employee2.Age + "wynik " + employee2.Result);
+Console.WriteLine(employee3.Name + "" + employee3.Surname + " lat " + employee3.Age + "wynik " + employee3.Result);
+Console.WriteLine();
+Console.WriteLine("------------------------------");
+Console.WriteLine("Najwiekszy wynik osiagnal pracownik :" + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " lat " + employeeWithMaxResult.Age + " wynik " + maxResult);
---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,33 @@
+﻿namespace ChallengeApp
+{
+    class Employee
+    {
+
+        private List<int> score = new List<int>();
+
+
+        public Employee(string login, string password, int age)
+        {
+            this.Name = login;
+            this.Surname = password;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
