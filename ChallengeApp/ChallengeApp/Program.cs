bcd2c05a0f2ba290b852c14cad6232f350652ce3
Praca <Praca@PRACA>
2024-07-10 16:07:52 +0200

    Delkaracje zmiennych oraz pierwsze ify

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Polska Górą");
+﻿string name = "Ewa";
+int age = 17;
+char sex = 'm';
+
+
+
+if (sex == 'k' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
